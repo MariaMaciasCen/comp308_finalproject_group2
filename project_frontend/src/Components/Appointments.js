b66828a9858b3cc0dfe57e35e356709750eb6c80
MariaMaciasCen <157546542+MariaMaciasCen@users.noreply.github.com>
2024-04-17 19:18:55 -0400

    Revert "Nurse funtionality"

--- project_frontend/src/Components/Appointments.js
+++ project_frontend/src/Components/Appointments.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { InlineWidget } from "react-calendly";
 
 const Appointments = () => {
-  return (
-    <div className="App">
-      <InlineWidget url="https://calendly.com/shawanagideon/nurse" />
-    </div>
-  );
+  return <div>Appointments Page</div>;
 };
 
 export default Appointments;
